35d4310cb6ff52a5588a8e8ba01bd1590ae7a662
David Gomez Muns <davidmuns@yahoo.es>
2022-07-03 21:23:20 +0200

    Update films.js

--- src/films.js
+++ src/films.js
@@ -1,23 +1,31 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result = array.map(movie => movie.director);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(movie => movie.director === director);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let movies = getMoviesFromDirector(array, director);
+  let averageScore = movies.reduce(
+    (acc, movie) => acc + movie.score, 0) / movies.length;
+  return parseFloat(averageScore.toFixed(3));
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let titles = [];
+  const movies = array.sort((a, b) => {
+    return a.title[0] === b.title[0] ? 0 : a.title[0] > b.title[0] ? 1 : -1;
+  })
+  titles = movies.map(movie => movies.title);
+  return titles;
 }
 
 // Exercise 5: Order by year, ascending
@@ -37,7 +45,7 @@ function hoursToMinutes() {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
